f1d38934c99ab218f720164673f3453c5ea7a213
mhamzaq869 <mhamzaq869@gmail.com>
2023-11-18 14:26:59 -0500

    init commit

---
+++ database/migrations/2023_09_13_075321_add_stablediffusion_default_model_to_settings_two_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('settings_two', function (Blueprint $table) {
+            if (Schema::hasColumn('settings_two', 'stablediffusion_default_model')) {
+                $table->string('stablediffusion_default_model')->default('stable-diffusion-512-v2-1')->change();
+            } else {
+                $table->string('stablediffusion_default_model')->default('stable-diffusion-512-v2-1');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('settings_two', function (Blueprint $table) {
+            $table->string('stablediffusion_default_model')->default('stable-diffusion-512-v2-1')->change();
+        });
+    }
+};
